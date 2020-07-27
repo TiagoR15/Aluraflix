1b3796f47f44d9c0706c1be716fd6c064cd45d0a
TheNumberGuy <tiagorocha1515@gmail.com>
2020-07-27 18:49:34 +0100

    feat: added ButtonLink component

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function ButtonLink(props) {
+  // props => { className: "" }
+  console.log(props);
+  return (
+    <a className={props.className} href={props.href}>
+      {props.children}
+    </a>
+  );
+}
+
+export default ButtonLink;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "../../assets/LogoAluraflix.png";
 
+import ButtonLink from "./components/ButtonLink";
+
 import "./menu.css";
 
 function Menu() {
@@ -9,6 +11,10 @@ function Menu() {
       <a href="/">
         <img className="Logo" src={Logo} alt="Aluraflix Logo" />
       </a>
+
+      <ButtonLink className="ButtonLink" href="/">
+        New Video
+      </ButtonLink>
     </nav>
   );
 }
