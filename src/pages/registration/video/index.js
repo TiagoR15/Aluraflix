7c9cf89e6b984936fed981bf59d2f3025ba8bff4
TheNumberGuy <tiagorocha1515@gmail.com>
2020-08-03 18:37:28 +0100

    feat: video registration complete

--- src/pages/registration/video/index.js
+++ src/pages/registration/video/index.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import PageDefault from "../../../components/PageDefault";
 import FormField from '../../../components/FormField';
 import useForm from '../../../hooks/useForm'
 import Button from "../../../components/Button"
 import createVideo from "../../../repositories/videos"
+import categoriesRepository from '../../../repositories/categories'
 
 function RegistrationVideo() {
   const history = useHistory()
-  const { values, handleChange, clearForm } = useForm({})
+  const [categories, setCategories] = useState([])
+  const { values, handleChange } = useForm({})
+
+  useEffect(() => {
+    categoriesRepository.getAll().then((response) => {
+      setCategories(response);
+    });
+  }, [])
+
+  console.log(categories);
+
   return (
     <PageDefault >
       <h1>Registration Video</h1>
 
       <form onSubmit={(event) => {
         event.preventDefault();
+
+        const categoryId = categories.find((categorie) => {
+          return categorie.titulo === values.categoria;
+        });
+
+        console.log(categoryId);
+
         createVideo.create({
-          titulo: values.url,
+          titulo: values.titulo,
           url: values.url,
-          categoriaId: 1
+          categoriaId: categoryId.id
         }).then(() => {
           console.log('Success');
           history.push('/');
